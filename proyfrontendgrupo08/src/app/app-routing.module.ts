4bc29cda7c85fe44dc9930e1e7971a220bc817c8
Mateo\Mateo <matuteloto42@gmail.com>
2022-06-23 21:39:38 -0300

    Creacion Clases Empleado, Dependencia y Formulario de alta Empleado

--- proyfrontendgrupo08/src/app/app-routing.module.ts
+++ proyfrontendgrupo08/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmpleadoFormComponent } from './components/empleado-form/empleado-form.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'formEmpleado',component:EmpleadoFormComponent},
+  {path:'**',pathMatch:'full',redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- proyfrontendgrupo08/src/app/app.module.ts
+++ proyfrontendgrupo08/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpleadoFormComponent } from './components/empleado-form/empleado-form.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadoFormComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
